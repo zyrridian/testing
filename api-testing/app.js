2eee01b4968d294a28acf2baeb6cf0517e0a5f0b
zyrridian <rezkyaf246@gmail.com>
2024-01-08 11:02:23 +0700

    test api ninja to generate random facts, and test puppeteer tiktok signin

---
+++ api-testing/app.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: '../.env' })
+
+// const apiUrl = 'https://jsonplaceholder.typicode.com/todos/1'
+const apiUrl = 'https://api.api-ninjas.com/v1/facts?limit=2'
+const headers = {
+    'Content-Type': 'application/json',
+    'X-Api-Key': process.env.API_NINJAS_KEY
+}
+
+async function fetchData() {
+    try {
+        const response = await axios.get(apiUrl, {headers})
+        const data = response.data
+        console.log('Data from API: ', data)
+    } catch (error) {
+        console.log('Error fetching data: ', error.message)
+    }
+}
+
+fetchData()
--- puppeteer-testing/testTiktok_SignIn.js
+++ puppeteer-testing/testTiktok_SignIn.js
@@ -1,48 +1,57 @@
-const puppeteer = require('puppeteer');
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const puppeteer = require('puppeteer')
+const dotenv = require('dotenv')
+dotenv.config({ path: '../.env'} );
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    // args:['--user-Sdata-dir=/user/data/directory/profile_n']
-    // Set to false to run in non-headless mode
-    // executablePath: '/path/to/chrome', // Provide the path to your Chrome/Chromium executable
-  });
+const app = express()
 
-  const loginMethod = ""
+app.get('/api/tiktok/signin', async (req, res) => {
 
-
-  if (loginMethod) {
+  try {
     
+    const userDataDir = '/Documents/programming/testing/puppeteer-testing'
+    const browser = await puppeteer.launch({
+      headless: false,
+      userDataDir: userDataDir
+    })
+
+    const isLogin = false
+    const page = await browser.newPage()
+
+    if (!isLogin) {
+
+      // await page.goto('https://tiktok.com/login')
+      // await page.evaluate(() => {
+      //   const rootDiv = document.querySelector('.tiktok-7pnfyo-DivLoginOptionContainer')
+      //   if (rootDiv) {
+      //     const childDivs = rootDiv.querySelectorAll('.tiktok-7u35li-DivBoxContainer')
+      //     if (childDivs.length > 1) {
+      //       childDivs[1].click()
+      //     }
+      //   }
+      // })
+
+      await page.goto('https://tiktok.com/login/phone-or-email/email')
+      await page.type('input[name="username"]', process.env.TIKTOK_USERNAME)
+      await page.type('input[type="password"]', process.env.TIKTOK_PASSWORD)
+      await page.click('button[type="submit"]')
+
+      // failed. cannot bypass slider rotation captcha
+
+
+
+      // await page.click('div[class="tiktok-7u35li-DivBoxContainer"]')
+
+    } else {
+
+    }
+
+  } catch (error) {
+    res.status(500).send(error.message)
   }
 
+})
 
-
-
-
-
-
-
-
-  const page = await browser.newPage();
-  await page.goto('https://www.tiktok.com/login');
-    await page.waitForSelector('.tiktok-7u35li-DivBoxContainer')
-    await page.click('.tiktok-7u35li-DivBoxContainer')
-  await page.waitForNavigation();
-  await page.screenshot({ path: 'screenshot_continue_with_google.png' });
-  await page.click('button[data-testid="btn-google"]');
-  await page.waitForNavigation();
-  await page.type('input[type="email"]', process.env.TIKTOK_EMAIL);
-  await page.click('button[type="next"]'); // Click the "Next" button
-  await page.waitForTimeout(5000); // Wait for a short time (adjust as needed)
-  await page.type('input[type="password"]', process.env.TIKTOK_PASSWORD);
-  await page.click('button[type="submit"]');
-  await page.screenshot({ path: 'screenshot_google_login_credentials.png' });
-  await page.waitForNavigation();
-  await page.goto('https://www.tiktok.com');
-  await page.waitForSelector('.main-content');
-  await page.screenshot({ path: 'main_page.png' });
-
-  // await browser.close();
-})();
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
